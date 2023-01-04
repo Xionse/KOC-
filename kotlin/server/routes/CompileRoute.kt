3f8c5e0f0ddb8e9510dc1ba654d0273dd7498f1e
Xionse <sexurexion@gmail.com>
2023-01-04 15:04:31 +0000

    Add the compileRoute

--- kotlin/server/routes/CompileRoute.kt
+++ kotlin/server/routes/CompileRoute.kt
@@ -23,3 +23,10 @@ private fun CompilerResult.toJson(): String {
 private fun escapeJson(str: String): String {
     return str.replace("\"", "\\\"").replace("\n", "\\n")
 }
+
+/*
+This CompileRoute class defines a route handler for the /compile URL that accepts a POST request with the Kotlin source code as the request body. It calls the compile function of the KotlinCompiler class to compile the source code, and returns a JSON response indicating whether the compilation was successful or not, along with the compiler output if the compilation was successful.
+
+The toJson extension function on CompilerResult is used to convert the CompilerResult object to a JSON string.
+
+*/
